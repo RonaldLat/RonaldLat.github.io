0171b6c06b6266b3067ed3d1cc80fdc6354d2653
Ronald Otieno <ronnlats@gmail.com>
2020-09-23 23:42:49 +0300

    styling the pages with colors and layout

--- app.js
+++ app.js
@@ -1,51 +1,4 @@
 
-// This  is the navigation's javascript
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    // const body = document.getElementsByTagName('body');
-    const bb = document.querySelector('body');
-
-
-
-    const logoBurger = document.querySelector('.logo');
-    // Toggle nav
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            // we use index and divide it by 7 to animate each menu item individually at different instances or delays
-
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index  / 7 + 0.3}s `;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-
-
-        //deactivating body scroll
-        // body.scrol
-
-
-
-        burger.classList.toggle('burger-active');
-        burger.classList.toggle('burger-line');
-        logoBurger.classList.toggle('logo-burger');
-        bb.classList.toggle('scroll-toggle');
-        // document.body.style.overflow= 'hidden';
-
-        // burger.addEventListener('click', () =>{
-        //     document.body.style.overflow= 'scroll';
-        // })
-    });
-
-
-};
 
 //bottom navigation
 let bottomNav = ()=>{
@@ -60,37 +13,45 @@ let bottomNav = ()=>{
     let aboutPage = document.querySelector('.about');
     let portofolioPage = document.querySelector('.portofolio');
     let contactPage = document.querySelector('.contact');
-     
+    let main = document.querySelector('main')
+    //all buttons
+    let allButtons = [home, about, portofolio, contact];
     //page tracker
     let pageTracker;
     //all pages array
     let allPages= [homePage, aboutPage, portofolioPage, contactPage];
 
-    homePage.classList.add('page-active')
-    home.addEventListener('click', function(){
-        if(pageTracker !==homePage){
-            pageTracker=homePage;
-            console.log(pageTracker)
+    //event listeners for all buttons
 
-        }
+    allButtons[0].addEventListener('click', ()=>{
+        
+    });
 
+    // homePage.classList.add('page-active')
+    // home.addEventListener('click', function(){
+    //     if(pageTracker !==homePage){
+    //         pageTracker=homePage;
+    //         console.log(pageTracker)
 
-    })
-    about.addEventListener('click', function(){
-        if(pageTracker !== aboutPage){
-            pageTracker=aboutPage;
-            console.log(pageTracker)
-        }
-
-    })
-    portofolio.addEventListener('click', function(){
-        // if(portofolioPage.classList.c)
+    //     }
 
 
-    })
-    contact.addEventListener('click', function(){
+    // })
+    // about.addEventListener('click', function(){
+    //     if(pageTracker !== aboutPage){
+    //         pageTracker=aboutPage;
+    //         console.log(pageTracker)
+    //     }
 
-    })
+    // })
+    // portofolio.addEventListener('click', function(){
+    //     // if(portofolioPage.classList.c)
+
+
+    // })
+    // contact.addEventListener('click', function(){
+
+    // })
 }
 
 
